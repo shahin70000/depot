a8c04a7e31e8eac9155e389f377d4f90b1621465
shahin70000 <shahin70000@gmail.com>
2012-07-01 16:23:38 +0300

    fixed the error in migration

--- db/migrate/20120701130749_create_products.rb
+++ db/migrate/20120701130749_create_products.rb
@@ -4,8 +4,7 @@ class CreateProducts < ActiveRecord::Migration
       t.string :title
       t.text :description
       t.string :imag_url
-      t.decimal :price, precision: 8, scale: 2,
-
+      t.decimal :price, precision: 8, scale: 2
       t.timestamps
     end
   end
