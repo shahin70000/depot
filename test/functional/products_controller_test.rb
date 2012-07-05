357b4173d882750015338defabd2e9b6c3092a87
shahin70000 <shahin70000@gmail.com>
2012-07-05 12:15:32 +0300

    ProductsControllerTest updated
    to match validation changes in the products model, this test was updated
    with valid values.
    
    Signed-off-by: shahin70000 <shahin70000@gmail.com>

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
+    @update = { title: 'Lorem Ipsum', description: 'Wibbles are fun!', imag_url: 'lorem.jpg', price: 19.95}
+    
   end
 
   test "should get index" do
@@ -18,7 +20,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, imag_url: @product.imag_url, price: @product.price, title: @product.title }
+      post :create, product: @update
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +37,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, imag_url: @product.imag_url, price: @product.price, title: @product.title }
+    put :update, id: @product.to_param, product: @update
     assert_redirected_to product_path(assigns(:product))
   end
 
