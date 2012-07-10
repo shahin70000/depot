1bb743a09931e357b4b6f3d1035adc29da5a06e1
shahin70000 <shahin70000@gmail.com>
2012-07-10 16:07:44 +0300

    faced a problem: "Rails mass assign"
    # this code from the book no longer works because of a change in
    # rails mass assignment; the following two lines are replacing:
    # @line_item = @cart.line_items.build(product: product)
    @line_item = @cart.line_items.build
    @line_item.product = product

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -41,11 +41,17 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(params[:line_item])
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+   # this code from the book no longer works because of a change in
+   # rails mass assignment; the following two lines are replacing:
+   # @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.line_items.build
+    @line_item.product = product
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
