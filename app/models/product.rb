456f42455b0a24422cbe0ac10efe8223b5bb3ed0
shahin70000 <shahin70000@gmail.com>
2012-07-11 12:02:28 +0300

    carts show view updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,6 +9,7 @@ class Product < ActiveRecord::Base
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :imag_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)$}i, message: 'must be a URL for GIF, JPG or PNG  image.'}
 
+  private
   # ensure that there are no line items referencing this product
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
