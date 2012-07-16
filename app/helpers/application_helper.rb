8f8c13848a8ec5429e593e095e69aab127aa8620
shahin70000 <shahin70000@gmail.com>
2012-07-16 13:04:00 +0300

    helper method hidden_div_if developed and used
    hidden_div_if(condition, attributes = {}, &block)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  # This method creates a div which will wrap your block
+  # note: the block comming after this method must be followed with <% end %>
+  #   to be wraped with div tag.
+  # @return [Object]
+  # @param [Object] condition
+  # @param [Object] attributes
+  # @param [Object] block
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"]="display: none"
+    end
+    content_tag("div",attributes,&block)
+  end
 end
